fb5b5c3ac3340e6cc0c49f3c72e5a72c3ae46486
Dhruv Parikh <dhruvparikhrg@gmail.com>
2020-05-28 19:26:48 +0530

    UI 2.1

--- js/therecipe.js
+++ js/therecipe.js
@@ -5,6 +5,8 @@ background-image: url('https://cdn.pixabay.com/photo/2017/02/15/10/39/salad-2068
   background-size: cover;
 
 */
+var cat_new;
+
 console.log('Yourmom')
 function displaytext(img){
 	img.style.background='url('+img.src+')';
@@ -20,7 +22,11 @@ function hidetext(img)
 	img.title = img.alt;
 	document.getElementById(img.id+'_caption').style.display='none'
 }
-
-function givesuggestion(cat){
-	console.log(cat)
+function showbutton(cat)
+{
+	document.getElementById('submit').style.display = 'block';
+	cat_new = cat;
+}
+function givesuggestion(){
+	console.log(cat_new)
 }
