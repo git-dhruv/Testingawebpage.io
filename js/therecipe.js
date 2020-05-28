4adc4714913041735e40ed80e3d1cdc5fb426bee
Dhruv Parikh <dhruvparikhrg@gmail.com>
2020-05-28 18:44:37 +0530

    UI 2.0

---
+++ js/therecipe.js
@@ -0,0 +1,24 @@
+/*
+background-image: url('https://cdn.pixabay.com/photo/2017/02/15/10/39/salad-2068220_960_720.jpg');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+
+*/
+console.log('Yourmom')
+function displaytext(img){
+	img.style.background='url('+img.src+')';
+	console.log(img)
+	img.style.filter = 'blur(2px)';
+	img.title = img.alt;
+	document.getElementById(img.id+'_caption').style.display='block'
+}
+
+function hidetext(img)
+{
+	img.style.background='url('+img.src+')';
+	console.log(img)
+	img.style.filter = '';
+	img.title = img.alt;
+	document.getElementById(img.id+'_caption').style.display='none'
+}
