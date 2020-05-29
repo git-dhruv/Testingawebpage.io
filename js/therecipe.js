b8aeebffed74873c6662dc9a4b40626162775d82
Dhruv Parikh <dhruvparikhrg@gmail.com>
2020-05-29 12:25:52 +0530

    Cateogory 4 established

--- js/therecipe.js
+++ js/therecipe.js
@@ -27,12 +27,53 @@ function showbutton(cat)
 	document.getElementById('submit').style.display = 'block';
 	cat_new = cat;
 }
-function givesuggestion(){
+var lists;
+function parse(){
+Papa.parse('https://raw.githubusercontent.com/git-dhruv/Testingawebpage.io/master/recipe_data.csv', {
+  download:true,
+  skipEmptyLines:'greedy',
+  header:true,
+  complete: function(results) {
+  suggest(results.data);
+  }
+  });
+}
+
+function suggest(data)
+{
+	var recipe_index=[];
+	var suggest_index;
+	console.log(data)
 	console.log(cat_new)
-	if(window.FileReader){
-		alert('Done')
+	for(i=0;i<data.length;i++)
+	{
+		let [index,cat,name] = Object.entries(data[i])
+		if(cat_new==cat[1])
+		{
+		console.log(name[1])
+		recipe_index.push(index[1])
+		}
 	}
-	else{
-		alert('File Reader is not supported! Use another Browser')
+	suggest_index = getRndInteger(0,recipe_index.length);
+	console.log(recipe_index)
+	for(i=0;i<data.length;i++)
+	{
+		let [index,cat,name] = Object.entries(data[i]);
+		if(index[1]==suggest_index)
+		{
+			console.log(name[1]);
+			document.getElementById('submit').style.display = 'block';
+			document.getElementById('ans').innerHTML = name[1];
+		}
 	}
 }
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+/*
+complete: function(results) {
+    window.lists = results.data;
+	console.log(window.lists);
+  }
+});*/
